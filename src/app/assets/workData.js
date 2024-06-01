42ddd291ffc3457b097d6dc4fd0a1b888847b478
Micah Jones <micahjones13@gmail.com>
2024-06-01 11:50:55 -0600

    add interests

--- src/app/assets/workData.js
+++ src/app/assets/workData.js
@@ -9,5 +9,33 @@ export const Data = [
     website: "https://astrouxds.com",
   },
 ];
-//TODO: postCSS plugin, astro in react(?) google sheets integration for feedback widget
-//TODO:
+//TODO: postCSS plugin, astro in react(?) google sheets integration for feedback widget, grafana plugin
+export const PastJobs = [
+  {
+    title: "Full Stack Software Engineer",
+    company: "Rocket Communications, Inc",
+    description: "",
+    timeFrame: "05/21 - Current",
+    relevantLinks: ["https://www.rocketcom.com"],
+  },
+];
+export const Hobbies = [
+  {
+    title: "Gaming",
+    description:
+      "I've been an avid gamer since I was a kid. The first game I fell in love with was The Legend of Zelda, Ocarina of Time. What a masterpiece. I've played most every Zelda sicne then. From there I played Halo: Combat Evolved on my Dad's old Mac and was introuduced to how fun FPS's can be. The entire Halo series has been very defining for me. More recently I've been playing a ton of Destiny 2 (around 3000 hours, let's say that's a testiment to my commitment). However, Baulders Gate 3 has been topped my favorite game of all time.",
+    relevantLinks: ["steam-profile-once-you-update-background"],
+  },
+  {
+    title: "Sports",
+    description:
+      "Along with gaming, sports were a huge part of my childhood. I played club soccer and started playing football in 4th grade. Football soon became my favorite, and I played up until my senior year in high school. I played quarterback until someone who could throw the ball much better than me came along, which is when I switched to slot reciever. I wasn't anything special but some of my fondest memories are from playing football.",
+    relevantLinks: [],
+  },
+  {
+    title: "Computers and Tech",
+    description:
+      "Because I got into gaming so early in my life, I've also been obsessed with technology. A few years ago I finally bought my first gaming level PC, in which I immediatley tried to play Skyrim with 3k+ mods (unsuccessfully, of course). Computer tech continues to be something I spend time on, always hoping to get an upgrade for my PC.",
+    relevantLinks: [],
+  },
+];
--- src/app/components/interests.js
+++ src/app/components/interests.js
@@ -1,8 +1,27 @@
+import { Hobbies } from "../assets/workData";
 export default function Interests() {
   return (
     <>
       <div className="border border-sky-500 m-auto p-4 flex flex-col">
         <h4>Intrests / Hobbies</h4>
+        {Hobbies.map((hobby, index) => {
+          return (
+            <div
+              key={index}
+              className="border border-sky-500 m-auto p-4 flex flex-col"
+            >
+              <h4>{hobby.title}</h4>
+              <p>{hobby.description}</p>
+              <div className="flex w-100 justify-end">
+                <ul className="flex justify-around items-center w-1/3">
+                  {hobby.relevantLinks.map((link, index) => {
+                    return <li key={index}>{link}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
